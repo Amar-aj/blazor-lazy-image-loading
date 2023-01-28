4e9b573130ce0c0ba627240bffb783ab56a9083b
Amar-aj <amar.aj.github@gmail.com>
2023-01-28 14:32:50 +0530

    blazor lazy image loading

---
+++ blazor-lazy-image-loading/Components/LazyImage.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace blazor_lazy_image_loading.Components;
+
+public partial class LazyImage
+{
+    [Parameter]
+    public string ImageUrl { get; set; }
+
+    [Parameter]
+    public int Width { get; set; }
+
+    [Parameter]
+    public int Height { get; set; }
+
+    [Parameter]
+    public string Alt { get; set; }
+
+    [Parameter]
+    public string CssClass { get; set; }
+
+    private ElementReference imageRef;
+
+    [Inject]
+    IJSRuntime JSRuntime { get; set; }
+
+    private async void LoadImage()
+    {
+        var src = await JSRuntime.InvokeAsync<string>("Interop.setImage", imageRef, ImageUrl);
+    }
+    private void OnError()
+    {
+        Console.WriteLine("Error loading image");
+    }
+}
